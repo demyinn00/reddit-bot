179c0d0a877ac00139bee3be681bb353ff65ad70
David Em-Yinn <demyinn@ucsd.edu>
2024-01-01 15:21:06 -0800

    Initial commit

---
+++ reddit_bot.py
@@ -0,0 +1,7 @@
+import os
+
+reddit = praw.Reddit(
+    client_id=os.getenv('REDDIT_CLIENT_ID'),
+    client_secret=os.getenv('REDDIT_CLIENT_SECRET'),
+    user_agent=os.getenv('REDDIT_USER_AGENT')
+)
